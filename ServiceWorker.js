b8060663bb651a483396a179312d05fe15ad6fdc
Maxwell Joseph Montemayor <maxwellmontemayor0@gmail.com>
2025-02-13 22:45:32 -0800

    720p

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Hop Harmony-1.0";
+const contentToCache = [
+    "Build/HopHarmony_WebGame.loader.js",
+    "Build/HopHarmony_WebGame.framework.js.unityweb",
+    "Build/HopHarmony_WebGame.data.unityweb",
+    "Build/HopHarmony_WebGame.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
